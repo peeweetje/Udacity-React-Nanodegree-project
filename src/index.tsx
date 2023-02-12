d8fec0c2b7122269a29f8c434bb9c8a19519bade
PEEWEE\petra <petravos70@gmail.com>
2023-02-12 20:17:34 +0100

    change the set-up to work with react 18.

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 import { GlobalStyle } from './globalStyle.styles';
 
-ReactDOM.render(<BrowserRouter><GlobalStyle/><App /></BrowserRouter>, document.getElementById('root'))
+const container = document.getElementById('root');
+const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+root.render(
+  <BrowserRouter>
+    <GlobalStyle />
+    <App />
+  </BrowserRouter>
+);
